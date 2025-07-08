1848bf66c48f03cc923332ef85347b943c0f5d4a
Avinier <adisubu.2410@gmail.com>
2025-07-08 14:47:32 +0530

    fix one

--- android/app/src/main/java/com/reactnativetest/MainApplication.kt
+++ android/app/src/main/java/com/reactnativetest/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ethinsights
+package com.reactnativetest
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/EthInsights/AppDelegate.swift
+++ ios/EthInsights/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "EthInsights",
+      withModuleName: "react-native-test",
       in: window,
       launchOptions: launchOptions
     )
